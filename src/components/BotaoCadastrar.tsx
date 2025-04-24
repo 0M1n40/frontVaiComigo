3b19ba2262f20368faa6038068fe3d669d3c27fd
Laura Carneiro de Azevedo <azevedolaura02@gmail.com>
2025-04-24 14:56:04 -0300

    feat(BotaoCadastrar): implement register button

---
+++ src/components/BotaoCadastrar.tsx
@@ -0,0 +1,17 @@
+function BotaoCadastrar() {
+
+    function register(){
+        console.log('implementar dps')
+    }
+
+    return (
+        <>
+            <button className="w-full max-w-[145px] h-[36px] border border-[#0D334D] text-[#0D334D] bg-transparent rounded-[10px] cursor-pointer" 
+            onClick={register}>
+                Cadastrar
+            </button>
+        </>
+    )
+}
+
+export default BotaoCadastrar
