72a402202ca146bdfe8d930fbd3a9a81ed9680f6
Laura Carneiro de Azevedo <azevedolaura02@gmail.com>
2025-04-24 14:52:32 -0300

    feat(BotaoLogin): implement login button

---
+++ src/components/BotaoLogin.tsx
@@ -0,0 +1,17 @@
+function BotaoLogin() {
+
+    function login(){
+        console.log('implementar dps')
+    }
+
+    return (
+        <>
+            <button className="w-full max-w-[145px] h-[36px] border border-[#0D334D] text-[#0D334D] bg-transparent rounded-[10px] cursor-pointer" 
+            onClick={login}>
+                Login
+            </button>
+        </>
+    )
+}
+
+export default BotaoLogin
