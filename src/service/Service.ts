c2cfb73fc1950776b3c68151a811c8aa3dec20fa
Beatriz Corrêa <bcorreacontato@gmail.com>
2025-04-25 14:11:56 -0300

    atualizando service

--- src/service/Service.ts
+++ src/service/Service.ts
@@ -1,9 +1,8 @@
 import axios from 'axios'
 
-const api = axios.create({
-    baseURL:'https://appcarona.onrender.com/'
+export const api = axios.create({
+    baseURL: import.meta.env.VITE_API_URL
 })
-
 
 export const cadastrarUsuario = async (url: string, dados: Object, setDados: Function) => {
     const resposta = await api.post(url, dados)
