d0ba9f1342fb89a11c2306049053fa5e01f678d3
Beatriz Corrêa <bcorreacontato@gmail.com>
2025-04-25 09:41:46 -0300

    atualizando app

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,16 @@
+import { BrowserRouter } from 'react-router-dom';
+import Navbar from './components/navbar/Navbar';
 
+function App() {
+  
+  return (
+    <>
+    <BrowserRouter>
+       <Navbar />
+       </BrowserRouter>
+    </>
+    
+  )
+}
+
+export default App;
