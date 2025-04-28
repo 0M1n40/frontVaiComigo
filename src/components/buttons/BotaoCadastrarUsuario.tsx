714aaead36601f03ea1adfe4061f4ce8d90af2c1
Laura Carneiro de Azevedo <azevedolaura02@gmail.com>
2025-04-28 14:41:14 -0300

    feat(BotaoCadastrarUsuario): update button

--- src/components/buttons/BotaoCadastrarUsuario.tsx
+++ src/components/buttons/BotaoCadastrarUsuario.tsx
@@ -1,17 +1,9 @@
-function BotaoCadastrarUsuario() {
-
-    function register(){
-        console.log('implementar dps')
-    }
-
+function BotaoCadastrarUsuario({ className }: { className?: string }) {
     return (
-        <>
-            <button className="w-full max-w-[145px] h-[36px] border border-[#0D334D] text-[#0D334D] bg-transparent rounded-[10px] cursor-pointer" 
-            onClick={register}>
-                Cadastrar
-            </button>
-        </>
+        <button className={`${className} w-full max-w-[145px] h-[36px] border border-[#0D334D] text-[#0D334D] bg-transparent rounded-[10px] cursor-pointer`}>
+            Cadastrar
+        </button>
     )
 }
 
-export default BotaoCadastrarUsuario
+export default BotaoCadastrarUsuario;
