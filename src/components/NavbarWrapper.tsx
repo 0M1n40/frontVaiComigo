2bce54d8a39a1c090e79525038be09628b04e05e
Laura Carneiro de Azevedo <azevedolaura02@gmail.com>
2025-04-28 10:00:50 -0300

    feat(NavbarWrapper): create navbar wrapper to hide navbar when on login or cadastro page

---
+++ src/components/NavbarWrapper.tsx
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
+
+function NavbarWrapper({ children }: { children: React.ReactNode }) {
+
+  const location = useLocation();
+  const [showNavbar, setShowNavbar] = useState(false);
+
+  useEffect(() => {
+    console.log('Location: ', location);
+
+    if (location.pathname === '/login' || location.pathname === '/cadastro') {
+      setShowNavbar(false);
+      return;
+    }
+    
+    setShowNavbar(true);
+  }, [location]);
+
+  return <div>{showNavbar && children}</div>;
+}
+
+export default NavbarWrapper;
