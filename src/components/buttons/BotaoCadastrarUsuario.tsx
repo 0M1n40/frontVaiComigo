0e6c158c33444da28e9b7eb7f11a6fadf7aba819
Laura Carneiro de Azevedo <azevedolaura02@gmail.com>
2025-04-28 15:04:41 -0300

    feat(BotaoCadastrarUsuario): add function that redirects user to register form when clicking on the button

--- src/components/buttons/BotaoCadastrarUsuario.tsx
+++ src/components/buttons/BotaoCadastrarUsuario.tsx
@@ -1,6 +1,16 @@
+import { useNavigate } from "react-router-dom";
+
 function BotaoCadastrarUsuario({ className }: { className?: string }) {
+
+    const navigate = useNavigate();
+
+    function handleClick() {
+        navigate('/cadastro');
+    }
+
     return (
-        <button className={`${className} w-full max-w-[145px] h-[36px] border border-[#0D334D] text-[#0D334D] bg-transparent rounded-[10px] cursor-pointer`}>
+        <button className={`${className} w-full max-w-[145px] h-[36px] border border-[#0D334D] text-[#0D334D] bg-transparent rounded-[10px] cursor-pointer`}
+        onClick={handleClick}>
             Cadastrar
         </button>
     )
